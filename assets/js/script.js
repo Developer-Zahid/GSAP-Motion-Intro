b2599421b2b8f95dcbd0ccffbd6ea289c9e0be05
tinyflow-dev <coderexpertzahid@gmail.com>
2026-09-03 14:27:09 +0600

    Add new scene elements and enhance animations for scene transitions

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,12 +80,94 @@ tl.fromTo(".scene-1",
     autoAlpha: 0,
     stagger: 0.1
 }, "<")
-.to(".scene-4__letter", {
+.to(".scene-4__circle", {
     "--_background-opacity": "0%",
     keyframes: [
         {yPercent: -50, duration: 0.5},
         {yPercent: 0, duration: 0.5, ease: "bounce.out",},
     ],
 }, "<-0.2")
+.set(".scene-4__circle", {
+    backgroundColor: "var(--color-light)",
+})
+.to(".scene-4__circle", {
+    duration: 0.6,
+    keyframes: [
+        {yPercent: 30},
+        {yPercent: -35},
+        {yPercent: 50},
+        {scaleX: 4.5, scaleY: 5, borderWidth: "0.18em", aspectRatio: "1 / 1", duration: 1,},
+    ],
+})
+.to(".scene-4__char", {
+    autoAlpha: 0,
+    duration: 0.1,
+}, "-=0.2")
+.to(".canvas__shutter", {
+    id: "scene-5",
+    autoAlpha: 1,
+    scale: 1,
+    duration: 0.05,
+})
+.to(".scene-4__circle", {
+    yPercent: -400,
+    borderWidth: "0.09em",
+    duration: 0.5,
+})
+.set(".scene-4__circle", {
+    backgroundColor: "var(--color-dark)",
+}, "<+0.2")
+.to(".scene-5", {
+    autoAlpha: 1,
+})
+.to(".scene-4__circle", {
+     "--_inner-cut-size": 1,
+    yPercent: 100,
+}, "<")
+.to(".canvas__shutter", {
+    "--_curve-height": "100%",
+    duration: 0.8,
+}, "<+0.2")
+.to(".scene-4__circle", {
+    xPercent: 400,
+    yPercent: 0,
+})
+.to(".scene-4__circle", {
+    xPercent: 0,
+    yPercent: -20,
+    autoAlpha: 0,
+})
+.to(".scene-5", {
+    color: "var(--color-dark)",
+}, "<")
+.fromTo(".scene-5__words",
+    {
+        x: "1em",
+    },
+    {
+        x: 0,
+        ease: "none",
+        keyframes: [
+            {text: "Designer.", duration: 0.5},
+            {text: "Developer.", delay: 0.5, duration: 0},
+            {text: "Partner.", delay: 0.5, duration: 0},
+        ],
+    },
+    "<+0.5"
+)
+.to(".canvas__shutter", {
+    autoAlpha: 0,
+    scale: 4,
+}, "<+0.2")
+.to(".scene-5", {
+    delay: 0.5,
+    fontSize: "0.5em",
+})
+.set(".scene-5__prefix", {
+    text: "Developer",
+}, "<+0.1")
+.set(".scene-5__words", {
+    text: "Zahid.",
+}, "<")
 
 GSDevTools.create();
